bafc7efade312dc2e16be6bbf29d4f83cc7ab5bd
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-08 18:05:04 -0500

    basic java calculator (unfinished)

---
+++ Calculator/Calculator.java
@@ -0,0 +1,29 @@
+package Calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        
+    }
+    
+    public int add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+    
+    public int subtract(int a, int b) {
+        int c = a - b;
+        return c;
+    }
+    
+    public int multiply(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+    
+    public int divide(int a, int b) {
+        int c = a / b;
+        return c;
+    }
+}
+
+`
