377d57846cccc8be3192b9df807a9928376d41e5
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-14 18:46:10 -0500

    java program to find the corresponding fibonacci at the n(th) position

---
+++ Week2Exercices/Fibonacci.java
@@ -0,0 +1,38 @@
+package Week2Exercices;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the fibonnaci position: ");
+        int position = scanner.nextInt();
+
+        System.out.println("That position of the fibonacci sequence is = " + getFibonnaci(position));
+
+    }
+
+    public static int getFibonnaci(int position) {
+
+        if (position <= 0) {
+            return 0;
+        }
+        if (position == 1 || position == 2) {
+            return 1;
+        }
+
+        int previous = 1;
+        int current = 1;
+        int result = 0;
+
+        for (int i = 3; i <= position; i++) {
+            result = current + previous;
+            previous = current;
+            current = result;
+        }
+
+        return result;
+    }
+}
+
