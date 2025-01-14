1170306825d199330abfc59b307a97a9bdbc544c
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-14 18:47:44 -0500

    Java program to find the maximum/minimum of 3 user inputted values

--- Week2Exercices/MaxMin.java
+++ Week2Exercices/MaxMin.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MaxMin {
     public static void main(String[] args) {
         
-        `Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the first number: ");
         float first = scanner.nextFloat();
