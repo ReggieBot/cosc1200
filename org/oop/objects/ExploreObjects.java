fd9aa27f012afb7ccf09b7f9d81a8ace34dc6078
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-15 17:48:40 -0500

    created main and student classes. Created a constructor for student class

---
+++ org/oop/objects/ExploreObjects.java
@@ -0,0 +1,7 @@
+package org.oop.objects;
+
+public class ExploreObjects {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ org/oop/objects/Student.java
@@ -0,0 +1,17 @@
+package org.oop.objects;
+
+import java.util.Formatter;
+
+public class Student {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
