da32c9bda4d111da7959de0751557e750d9f7c87
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-15 18:07:26 -0500

    called constructor method and passed values to it, then printed values to terminal

--- org/oop/objects/ExploreObjects.java
+++ org/oop/objects/ExploreObjects.java
@@ -2,6 +2,10 @@ package org.oop.objects;
 
 public class ExploreObjects {
     public static void main(String[] args) {
-
+        Student s1 = new Student("10088031", "Reggie", "Brown", 20);
+        System.out.println("Students first name is " + s1.getFirstName());
+        System.out.println("Students last name is " + s1.getLastName());
+        System.out.println("Student ID = " + s1.getId());
+        System.out.println("Students age is " + s1.getAge());
     }
 }
