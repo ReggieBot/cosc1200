1295580eaecdb0b6512fc199961670553a62f8b7
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-20 18:00:58 -0500

    created main class

---
+++ Concepts/Inheritance/Main.java
@@ -0,0 +1,9 @@
+package Concepts.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", "Golden Retriever");
+        dog.bark();
+        dog.eat();
+    }
+}
