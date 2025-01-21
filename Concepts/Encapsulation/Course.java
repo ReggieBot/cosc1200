0ff2976a81138fddcc444b9ee90fe9ffea3a78b2
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-21 18:08:05 -0500

    created setter methods

--- Concepts/Encapsulation/Course.java
+++ Concepts/Encapsulation/Course.java
@@ -25,4 +25,24 @@ public class Course {
     public int getCurrentStudents() {
         return currentStudents;
     }
+
+    // Method to add a student to the course
+    // Includes validation to check if the course is currently full
+    public boolean addStudent() {
+        if (currentStudents < maxStudents) {
+            currentStudents++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeStudent() {
+        if (currentStudents > 0) {
+            currentStudents--;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
