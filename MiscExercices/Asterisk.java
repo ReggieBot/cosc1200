ed515f614f8c8894a257334409593f6f6fa3b26b
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-27 18:26:03 -0500

    Working state, does not print spaces

---
+++ MiscExercices/Asterisk.java
@@ -0,0 +1,18 @@
+package MiscExercices;
+import java.util.Scanner;
+
+public class Asterisk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows in the triangle? ");
+        int rows = scanner.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
