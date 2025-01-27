30f2cee070b1b4e065ccc509c6986c2818bcb872
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-27 18:29:42 -0500

    added spaces - working state

--- MiscExercices/Asterisk.java
+++ MiscExercices/Asterisk.java
@@ -7,8 +7,13 @@ public class Asterisk {
         System.out.println("How many rows in the triangle? ");
         int rows = scanner.nextInt();
         for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++)
+                System.out.print(" ");
+
+
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
 
             }
             System.out.println();
