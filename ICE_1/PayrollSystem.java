8f1e8f8e1d95cd845cd8bc6116a6e3fd8d237da1
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-29 18:17:02 -0500

    Created the addEmployee method

--- ICE_1/PayrollSystem.java
+++ ICE_1/PayrollSystem.java
@@ -2,10 +2,27 @@ package ICE_1;
 import java.util.Scanner;
 
 public class PayrollSystem {
-    // Initializing array and scanner object
-    private Employee[] employees;
+    // Initializing employee variables 
+    private Employee employee1;
+    private Employee employee2;
+    private Employee employee3;
+    private Employee employee4;
+    private Employee employee5;
     private int employeeCount;
-    Scanner scanner = new Scanner(System.in);
-    
+
+    public PayrollSystem() {
+        employeeCount = 0;
+    }
+
+    // Adds employee to the payroll system
+    public void addEmployee() {
+        if (employeeCount >= 5) {
+            System.out.println("Max Employees reached");
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter employee details: ");
+        
+    }
     
 }
