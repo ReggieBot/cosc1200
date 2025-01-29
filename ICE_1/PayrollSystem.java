235e6d0ae4957e642dfc26a2483d79f7df4e7eda
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-29 18:04:20 -0500

    Created PayrollSystem.java

---
+++ ICE_1/PayrollSystem.java
@@ -0,0 +1,11 @@
+package ICE_1;
+import java.util.Scanner;
+
+public class PayrollSystem {
+    // Initializing array and scanner object
+    private Employee[] employees;
+    private int employeeCount;
+    Scanner scanner = new Scanner(System.in);
+    
+    
+}
