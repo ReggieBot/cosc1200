99e1fe075f87615ba6d6f32341d15014b050d1b6
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-30 03:35:24 -0500

    Created Student.java. Initialized variables, created constructor, created displayDetails method

---
+++ Assignment1/Student.java
@@ -0,0 +1,23 @@
+package Assignment1;
+
+public class Student {
+    // Initializing variables
+    String name;
+    int age;
+    String studentID;
+
+    // Constructor
+    public Student(String name, int age, String studentID) {
+        this.name = name;
+        this.age = age;
+        this.studentID = studentID;
+    }
+    
+    // Displays student details
+    public void displayDetails() {
+        System.out.println("---Student Details---")
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + studentID);
+    }
+}
