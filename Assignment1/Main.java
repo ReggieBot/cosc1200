450abeae1706bba2dafebf017bddb28400efb08c
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-30 03:40:45 -0500

    Created main method, instantiated 2 student objects, called displayDetails() on both objects

---
+++ Assignment1/Main.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating student objects
+        Student student1 = new Student("Reggie", 20, "100883021");
+        Student student2 = new Student("Ben", 22, "100883021");
+
+        // Displaying student details
+        student1.displayDetails();
+        student2.displayDetails();
+    }
+    
+}
