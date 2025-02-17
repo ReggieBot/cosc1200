f0252b844946436d98b6a12681b5041520019420
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-02-17 11:11:55 -0500

    Added comments

---
+++ Assignment2/IntegerArray.java
@@ -0,0 +1,22 @@
+// Reggie Brown
+// 2/17/25
+// COSC1200 - Professor Tauhid
+// Assignment 2
+// DESC: This program will take in a user input of integers and store them in an array
+// This will then perform many operations on the array such as:
+// Printing the array, finding the largest, smallest, sum, and average of the array
+// There will also be an option to search for a number and check if it exists in the array
+// It will also perform a bubble sort and print the sorted array
+
+
+
+
+package Assignment2;
+
+import java.util.Scanner;
+
+public class IntegerArray {
+    public static void main(String[] args) {
+
+    }
+}
