d7fa3c98a0d03d2679c1f72c6e908ae5d1d47b12
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-10 15:18:10 -0400

    Completed FindMax class

--- Assignment3/FindMax.java
+++ Assignment3/FindMax.java
@@ -1,8 +1,23 @@
 package Assignment3;
 
 // This class shows an example of taking an array as a parameter
-// It then iterates through the array to find the maximum value
+// It then iterates through the array to find the maximum integer
 
 public class FindMax {
     
+    public static int findMax(int[] array) {
+        
+        // Initialize the max value to the first element of the array
+        // Iterate through the array and update the max value if a larger int is found
+        // Stealing this from a previous assignment we did
+
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
