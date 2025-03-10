3a629786af7b61a609b01896cec6fd421d729269
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-10 15:44:33 -0400

    Completed StringReverse class

--- Assignment3/StringReverse.java
+++ Assignment3/StringReverse.java
@@ -11,9 +11,17 @@ package Assignment3;
 public class StringReverse {
     
     public static String stringReverse(String str) {
-        
-        // Defining base cases
+
+        // Defining base cases and terminating conditions
         // If sring is empty || string has only one char
-        if 
+        if (str.length() == 0 || str.length() == 1) {
+            return str;
+        }
+
+        // Recursion
+        // Takes first character of string and puts it at the end
+        // Then calls the method again with the substring starting from the second character
+
+        return stringReverse(str.substring(1)) + str.charAt(0);
     }
 }
