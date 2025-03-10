debf848cad5cd07f216975a58b94190f6720302a
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-10 15:34:38 -0400

    Finished comments and added references, started building the method

--- Assignment3/StringReverse.java
+++ Assignment3/StringReverse.java
@@ -3,6 +3,17 @@ package Assignment3;
 // This class is used to demonstrate recursion
 // It uses a method to reverse a string
 
+// ---- References ----
+// https://www.w3schools.com/java/ref_string_substring.asp
+// https://www.w3schools.com/java/ref_string_charat.asp
+// https://www.geeksforgeeks.org/reverse-a-string-using-recursion/
+
 public class StringReverse {
     
+    public static String stringReverse(String str) {
+        
+        // Defining base cases
+        // If sring is empty || string has only one char
+        if 
+    }
 }
