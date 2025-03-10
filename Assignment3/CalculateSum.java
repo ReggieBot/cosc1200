29070a27e096440167a7c7c2863a33f74d062556
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-10 15:05:43 -0400

    Finished calculateSum class

--- Assignment3/CalculateSum.java
+++ Assignment3/CalculateSum.java
@@ -1,5 +1,10 @@
 package Assignment3;
 
+// This class is used to calculate the sum of two integers
+
 public class CalculateSum {
     
+    public static int calculateSum(int num1, int num2) {
+        return num1 + num2;
+    }
 }
