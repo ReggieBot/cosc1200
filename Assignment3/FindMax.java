e0eb9ac16c9e5a02f11ed79deb112538a26ba68b
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-10 15:15:09 -0400

    Initial comments and class setup

--- Assignment3/FindMax.java
+++ Assignment3/FindMax.java
@@ -1,5 +1,8 @@
 package Assignment3;
 
+// This class shows an example of taking an array as a parameter
+// It then iterates through the array to find the maximum value
+
 public class FindMax {
     
 }
