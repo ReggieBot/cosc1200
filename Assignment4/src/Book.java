25124663a53237debb2d4d6da64c30b8109eaab1
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-07 09:40:20 -0400

    Abstract Book.java class created

---
+++ Assignment4/src/Book.java
@@ -0,0 +1,32 @@
+package Assignment4.src;
+
+public abstract class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int yearPublished;
+
+    // Constructor
+    public Book(String title, String author, String isbn, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.yearPublished = yearPublished;
+    }
+
+    // Abstract method to get book type
+    // This method will be implemented in subclass
+    public abstract void get_book_type();
+
+    // Print book details
+    public void display_info() {
+        System.out.println("-------------------------");
+        System.out.println("-------Book Details------");
+        System.out.println("-------------------------");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("-------------------------");
+    }
+}
