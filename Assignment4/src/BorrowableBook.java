5dab023ab811545b1fbb3eec26e38cc345bf7597
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-07 15:51:46 -0400

    Updated BorrowableBook class to reflect main vars

--- Assignment4/src/BorrowableBook.java
+++ Assignment4/src/BorrowableBook.java
@@ -4,28 +4,28 @@ public class BorrowableBook extends Book implements Borrowable{
     // Flag to check if book is borrowed
     private boolean isBorrowed = false;
     // Composition to create a reference to the book (SEE README / REFERENCES)
-    private Book referenceBook;
+    private Book borrowableBook;
 
     // Constructor
-    public BorrowableBook(Book referenceBook) {
-        super(referenceBook.getTitle(), referenceBook.getAuthor(), referenceBook.getIsbn(), referenceBook.getYearPublished());
-        this.referenceBook = referenceBook;
+    public BorrowableBook(Book borrowableBook) {
+        super(borrowableBook.getTitle(), borrowableBook.getAuthor(), borrowableBook.getIsbn(), borrowableBook.getYearPublished());
+        this.borrowableBook = borrowableBook;
     }
 
     @Override
     public String get_book_type() {
-        return referenceBook.get_book_type();
+        return borrowableBook.get_book_type();
     }
 
     @Override
     public void borrow() {
-        if (referenceBook instanceof ReferenceBook) {
-            System.out.println("Referene Books are not borrowable. Book: " + referenceBook.getTitle());
+        if (borrowableBook instanceof ReferenceBook) {
+            System.out.println("Referene Books are not borrowable. Book: " + borrowableBook.getTitle());
         } else if (!isBorrowed) {
             isBorrowed = true;
-            System.out.println("Successfully Borrowed Book: " + referenceBook.getTitle());
+            System.out.println("Successfully Borrowed Book: " + borrowableBook.getTitle());
         } else {
-            System.out.println("Sorry, that book is already borrowed: " + referenceBook.getTitle());
+            System.out.println("Sorry, that book is already borrowed: " + borrowableBook.getTitle());
         }
     }
 
@@ -33,15 +33,15 @@ public class BorrowableBook extends Book implements Borrowable{
     public void returnBook() {
         if (isBorrowed) {
             isBorrowed = false;
-            System.out.println("Thank you, book returned successfully: " + referenceBook.getTitle());
+            System.out.println("Thank you, book returned successfully: " + borrowableBook.getTitle());
         } else {
-            System.out.println("This book has not been borrowed: " + referenceBook.getTitle());
+            System.out.println("This book has not been borrowed: " + borrowableBook.getTitle());
         }
     }
 
     @Override
     public void display_info() {
-        referenceBook.display_info();
+        borrowableBook.display_info();
         if (isBorrowed) {
             System.out.println("Borrow Status: Yes");
         } else {
@@ -56,6 +56,6 @@ public class BorrowableBook extends Book implements Borrowable{
     }
 
     public Book getReferenceBook() {
-        return referenceBook;
+        return borrowableBook;
     }
 }
