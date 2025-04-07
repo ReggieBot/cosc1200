f88ea9f4b1a1e522af092d2c04b6c3a218830414
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-07 09:58:52 -0400

    Created and implemented FictionBook.java. This class is a subclass of the Book superclass

---
+++ Assignment4/src/FictionBook.java
@@ -0,0 +1,18 @@
+package Assignment4.src;
+
+public class FictionBook extends Book {
+    private String genre;
+    
+    // Constructor
+    public FictionBook(String title, String author, String isbn, int yearPublished, String genre) {
+        super(title, author, isbn, yearPublished);
+        this.genre = genre;
+    }
+
+    // Implementing the abstract method from Book class
+    // Using @Override annotation to show that this method is overriding a method from its superclass
+    @Override
+    public String get_book_type() {
+        return "Fiction";
+    }
+}
